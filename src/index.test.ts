cdc7ee77c24457ab5720ccf5c50c8a60479f508f
Ken Powers <ken@kenpowers.net>
2022-02-23 20:43:40 -0600

    Add Bootstrap breakpoints

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,22 @@
-import foo from './';
+import def, { bootstrap, bootstrapQueries } from './';
 
-test('foo', () => {
-  expect(foo).toBe('foo');
+test('default export', () => {
+  expect(def).toEqual({ lg: 960, md: 720, sm: 540, xl: 1140, xxl: 1320 });
+  expect(def).toBe(bootstrap);
+});
+
+describe('queries', () => {
+  test.each([
+    ['default', def, bootstrapQueries],
+    ['bootstrap', bootstrap, bootstrapQueries],
+  ])('%s', (_name, numbers, queries) => {
+    expect(queries).toEqual(
+      Object.fromEntries(
+        Object.entries(numbers).map(([key, num]) => [
+          key,
+          `(min-width: ${num}px)`,
+        ]),
+      ),
+    );
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export default 'foo';
+export const bootstrap = {
+  sm: 540,
+  md: 720,
+  lg: 960,
+  xl: 1140,
+  xxl: 1320,
+};
+
+export default bootstrap;
+
+export const bootstrapQueries = {
+  sm: '(min-width: 540px)',
+  md: '(min-width: 720px)',
+  lg: '(min-width: 960px)',
+  xl: '(min-width: 1140px)',
+  xxl: '(min-width: 1320px)',
+};
